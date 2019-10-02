eb98437ac4ff1127fc6e97a523ee8170b9077c6a
stacy <chiufin@hotmail.com>
2019-10-02 22:53:45 +0100

    show from and to currency, handle change dropdown value

--- src/CurrencyBox.jsx
+++ src/CurrencyBox.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { getCurrencyList, getRate } from './apis';
 
 function CurrencyBox({from='USD', to='EUR'}) {
+    const [fromCurrency, setFromCurrency] = useState(from);
+    const [toCurrency, setToCurrency] = useState(to);
     const [rate, setRate] = useState(1);
     const [reversedRate, setReversedRate] = useState(1); 
     const [list, setList] = useState([]);
@@ -15,27 +17,37 @@ function CurrencyBox({from='USD', to='EUR'}) {
             setRate(rate)
             setReversedRate(rate.toFixed(6))
         })
-
     }, [from, to])
+
+    const changeCurrency = ({ target : { value }}, whichCurrency) => {
+        switch(whichCurrency){
+            case 'from':
+                setFromCurrency(value);
+                break;
+            case 'to':
+                setToCurrency(value);
+                break;
+            default:
+                break;
+        }
+    }
   return (
     <>
         <div>
-            <p>1  = {rate} </p>
-            <select>
+            <p>1 {fromCurrency}= {rate} {toCurrency}</p>
+            <select value={ fromCurrency }
+                    onChange={e =>changeCurrency( e, 'from')}>
                 {list.map(each =>
-                    <option key={each} 
-                            selected={ each === from }>{each}
-                    </option>
+                    <option key={each}>{each}</option>
                 )}
             </select>
         </div>
         <div>
-            <p>1  = {reversedRate} </p>
-            <select>
+            <p>1 {toCurrency}= {reversedRate} {fromCurrency}</p>
+            <select value={ toCurrency } 
+                    onChange={e =>changeCurrency( e, 'to')}>
                     {list.map(each =>
-                        <option key={each} 
-                                selected={ each === to }>{each}
-                        </option>
+                        <option key={each}>{each}</option>
                     )}
             </select>
         </div>
