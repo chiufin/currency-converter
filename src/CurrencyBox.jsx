72a7e22923aaf7270dd8d9d2a8bb97fd079d20fd
stacy <chiufin@hotmail.com>
2019-10-03 01:36:30 +0100

    style prototype

--- src/CurrencyBox.jsx
+++ src/CurrencyBox.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { getCurrencyList, getRate } from './apis';
 
-function CurrencyBox({from='USD', to='EUR'}) {
+const CurrencyBox = ({from='USD', to='EUR'}) => {
     const [fromCurrency, setFromCurrency] = useState(from);
     const [toCurrency, setToCurrency] = useState(to);
     const [rate, setRate] = useState(1);
     const [list, setList] = useState([]);
+    const [amount, setAmount] = useState(1);
 
     useEffect(() => {
         getCurrencyList()
@@ -29,28 +30,60 @@ function CurrencyBox({from='USD', to='EUR'}) {
                 break;
         }
     }
+
+    const changeAmount =  ({ target : { value }}, whichCurrency ) => {
+        switch(whichCurrency){
+            case 'from':
+                setAmount(value);
+                break;
+            case 'to':
+                setAmount(value*(1/rate).toFixed(6));
+                break;
+            default:
+                break;
+        }
+    }
   return (
-    <>
-        <div>
-            <p>1 {fromCurrency}= {rate} {toCurrency}</p>
-            <select value={ fromCurrency }
+    <div className="CurrencyBox-container">
+        <OneBox from={fromCurrency} 
+                to={toCurrency} 
+                fromFullName={fromCurrency}
+                rate={rate}
+                list={list}
+                amount={amount}
+                changeCurrency={changeCurrency}
+                changeAmount={changeAmount}/>
+        <OneBox from={toCurrency} 
+                to={fromCurrency} 
+                fromFullName={toCurrency}
+                rate={(1/rate).toFixed(6)}
+                list={list}
+                amount={amount*rate}
+                changeCurrency={changeCurrency}
+                changeAmount={changeAmount}/>
+    </div>
+  );
+}
+
+const OneBox = ({from, to, fromFullName, rate, list, amount, changeCurrency, changeAmount}) => 
+<div className="CurrencyBox">
+    <p className="CurrencyBox-label">1 {from} = {rate} {to}</p>
+    <div className="CurrencyBox-input">
+        <div className="CurrencyBox-input-dropdown-container">
+            <select className="CurrencyBox-input-dropdown"
+                    value={ fromFullName }
                     onChange={e =>changeCurrency( e, 'from')}>
                 {list.map(each =>
                     <option key={each}>{each}</option>
                 )}
             </select>
         </div>
-        <div>
-            <p>1 {toCurrency}= {(1/rate).toFixed(6)} {fromCurrency}</p>
-            <select value={ toCurrency } 
-                    onChange={e =>changeCurrency( e, 'to')}>
-                    {list.map(each =>
-                        <option key={each}>{each}</option>
-                    )}
-            </select>
-        </div>
-    </>
-  );
-}
+        <input 
+            className="CurrencyBox-input-text"
+            type="text" 
+            value={amount}>
+        </input>
+    </div>
+</div>
 
 export default CurrencyBox;
